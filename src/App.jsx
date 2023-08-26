b504a05f8e9e8b1b95d2b7cb61557466b7eea86d
KirilDora <kirildora02@gmail.com>
2023-08-26 13:59:09 +0300

    completed website accroding to guide

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import SideBar from './components/SideBar';
+import ChannelBar from './components/ChannelBar'
+import ContentContainer from './components/ContentContainer';
+
+function App() {
+  return (
+    <div className='flex '>
+      <SideBar />
+      <ChannelBar />
+      <ContentContainer />
+    </div>
+  );
+}
+
+export default App;
